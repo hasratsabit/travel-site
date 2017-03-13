e554cbe449c8509760d3026a7b0457d2f7205d7d
Hasrat <hsabit111@gmail.com>
2017-03-12 23:22:00 -0400

    new style added

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,11 +5,12 @@ var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
 var mixins = require('postcss-mixins');
+var hexrgba = require('postcss-hexrgba');
 
 // Src, Dest, and Pipe
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
     // This avoids server-stop when there is an error
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString());
