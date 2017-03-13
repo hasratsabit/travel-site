d2e2269c6f2e07f5772a5b6450ba38b4b2a46741
Hasrat <hsabit111@gmail.com>
2017-03-13 16:24:34 -0400

    Reveal animation is added

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,4 +1,12 @@
 
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from "jquery";
 
-let mobileMenu = new MobileMenu()
+let mobileMenu = new MobileMenu();
+
+// We use the constructor in to dynamically reveal any part of the page we want.
+// The rest of the code in the reveal file is recyclible except the offset and elements differ
+// We pass the element path and offset we want the items to reveal.
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "65%");
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,35 @@
+import $ from "jquery";
+// This library is used for scrolling the window
+import waypoints from "../../../../node_modules/waypoints/lib/noframework.waypoints"
+
+class RevealOnScroll {
+
+  constructor(element, offset) {
+    this.itemsToReveal = element // It gets the element string from the app.js
+    this.hideInitially();
+    this.offsetPercentage = offset; // This variable needs to be hoisted first before the createwaypoint method fire.
+    this.createWayPoints();
+  }
+
+  hideInitially() {
+    this.itemsToReveal.addClass('reveal-item');
+  }
+
+  // For this method to fire as soon as window loads, it should be called inide the constructor.
+  createWayPoints() {
+    let that = this; // We want the this keyword to point to the constructor itself when we use it with offset. We store the value in this level in variable so we use the varaible blow instead the keword itself. The keyword itself points to the inner object.
+    this.itemsToReveal.each(function() {
+      var currentItem = this; // The this points to the itemsToReveal. So it is assigned to a variable to use in method bellow. This this by itself inside the method will point to the object itself
+      new Waypoint({ // This constructor is coming from the library imported above.
+        element: currentItem,
+        handler: function() {
+          $(currentItem).addClass("reveal-item--is-visible");
+        },
+        offset: that.offsetPercentage // This variable is assigned to offset parameter that gets its value from the instance in the app.js
+      })
+    })
+  }
+
+}
+
+export default RevealOnScroll;
